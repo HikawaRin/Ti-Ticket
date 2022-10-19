3ce16f773a697eccc7bfe2533ea7ae9295aba7e5
Hikawa Rin <HikawaRin@outlook.com>
2022-10-19 15:19:19 +0000

    Implement secret util

---
+++ utils/secret_util.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"math/rand"
+	"time"
+)
+
+var (
+	min int = '0'
+	max int = 'z'
+)
+
+func GetSecret(len int) string {
+	rand.Seed(time.Now().Unix())
+	secret := ""
+	for i := 0; i < len; i++ {
+		secret = secret + string(byte(rand.Intn(max-min) + min))
+	}
+	return secret
+}
